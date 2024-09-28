0b6783466e131df4103aa4232fa6c37b35853f55
KJagadeeswarReddy <142029358+KJagadeeswarReddy@users.noreply.github.com>
2024-09-28 18:32:25 +0000

    Changes to be committed: 	modified:   index.html   	new file:   script.js 	modified:   styles.css

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent!');
+    // Here you can add code to send the form data to a server
+});
